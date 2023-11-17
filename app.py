ef80643954a1d03df42c6c883a63ac127053f697
Nithin G <79520840+2nithing@users.noreply.github.com>
2023-11-17 09:18:11 +0000

    name printing only

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    print('let\'s print')
-    return ('Hi')
+    
+    return render_template('index.html')
+
+@app.route('/name', methods = ['GET','POST'])
+def name():
+    if request.method == 'POST':
+        names = request.form['firstname']
+        print(names)
+        return render_template('name.html', names=names)
+    else:
+        return render_template('name.html')
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
